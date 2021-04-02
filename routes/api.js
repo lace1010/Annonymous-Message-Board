384ab2b908574552aa75292f2460d4f398999614
Hunter Lacefield <lace1010@gmail.com>
2021-04-02 11:27:07 +0800

    add connection log to see if db is connected to site

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,23 @@
-'use strict';
+"use strict";
+const mongoose = require("mongoose");
 
 module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+  let uri = process.env.ANONYMOUS_MESSAGE_BOARD_MONGO_URI;
+  mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
+  const Schema = mongoose.Schema;
+  // Use the next four lines to see if you are conneted to mongoose correctly
+  var db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error:"));
+  db.once("open", () => {
+    console.log("Connection Successful!");
+  });
 
+  app.route("/api/threads/:board");
+
+  app.route("/api/replies/:board");
 };
